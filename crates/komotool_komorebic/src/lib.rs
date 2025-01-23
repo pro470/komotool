930e92d7667fbb87152a499bb5bebbe29ca2c32e
pro470 <reynoldsadjei@gmx.de>
2025-01-23 23:46:58 +0100

    refactor: Update imports and plugin implementation in komotool_komorebic

--- crates/komotool_komorebic/src/lib.rs
+++ crates/komotool_komorebic/src/lib.rs
@@ -1,12 +1,14 @@
-use bevy::*;
+use bevy::prelude::*;
+use komorebi_client::{send_message, send_batch, SocketMessage, NotificationEvent, Notification};
+use bevy_mod_scripting::core::bindings::function::namespace::NamespaceBuilder;
 
 pub struct KomoToolKomorebicPlugin; 
 
 impl Plugin for KomoToolKomorebicPlugin {
 
-    fn build(app: &mut App) {
+    fn build(&self, app: &mut App) {
+
+
 
     }
-
-    
 }
