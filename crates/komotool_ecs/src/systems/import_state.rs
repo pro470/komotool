7f4c62d801f40f47081889eb516e256321ac8bc4
pro470 <reynoldsadjei@gmx.de>
2025-02-08 21:40:37 +0100

    refactor: Update monitor entity creation to use getter methods

--- crates/komotool_ecs/src/systems/import_state.rs
+++ crates/komotool_ecs/src/systems/import_state.rs
@@ -16,21 +16,21 @@ pub fn import_komorebi_state(
     let state: State = serde_json::from_str(&send_query(&SocketMessage::State).unwrap()).unwrap();
 
     // Spawn new monitor entities
-    for (idx, komo_mon) in state.monitors.elements.iter().enumerate() {
+    for (idx, komo_mon) in state.monitors.elements().iter().enumerate() {
         let mut entity = commands.spawn(Monitor {
-            id: komo_mon.id,
-            name: komo_mon.name.clone(),
-            device: komo_mon.device.clone(),
-            device_id: komo_mon.device_id.clone(),
-            serial_number_id: komo_mon.serial_number_id.clone(),
-            physical_size: komo_mon.size.into(),
-            work_area_size: komo_mon.work_area_size.into(),
-            work_area_offset: komo_mon.work_area_offset.into(),
-            window_based_work_area_offset: komo_mon.window_based_work_area_offset.into(),
-            window_based_work_area_offset_limit: komo_mon.window_based_work_area_offset_limit,
+            id: komo_mon.id(),
+            name: komo_mon.name().clone(),
+            device: komo_mon.device().clone(),
+            device_id: komo_mon.device_id().clone(),
+            serial_number_id: komo_mon.serial_number_id().clone(),
+            physical_size: komo_mon.size().into(),
+            work_area_size: komo_mon.work_area_size().into(),
+            work_area_offset: komo_mon.work_area_offset().into(),
+            window_based_work_area_offset: komo_mon.window_based_work_area_offset().into(),
+            window_based_work_area_offset_limit: komo_mon.window_based_work_area_offset_limit(),
         });
 
-        if idx == state.monitors.focused {
+        if idx == state.monitors.focused_idx() {
             entity.insert(Focused(1));
         }
     }
@@ -39,7 +39,7 @@ pub fn import_komorebi_state(
     *app_state = AppState {
         is_paused: state.is_paused,
         resize_delta: state.resize_delta,
-        new_window_behaviour: state.new_window_behaviour,
+        //new_window_behaviour: state.new_window_behaviour,
         float_override: state.float_override,
         cross_monitor_move_behaviour: state.cross_monitor_move_behaviour,
         unmanaged_window_operation_behaviour: state.unmanaged_window_operation_behaviour,
