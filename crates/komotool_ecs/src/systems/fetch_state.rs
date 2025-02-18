8f04c084aae3853a67fb1c1f42fe9580b822d5cb
pro470 <reynoldsadjei@gmx.de>
2025-02-18 19:42:09 +0100

    refactor: replace the fetch_state function with update_komorebi_state_from_notifications

--- crates/komotool_ecs/src/systems/fetch_state.rs
+++ crates/komotool_ecs/src/systems/fetch_state.rs
@@ -1,12 +1,14 @@
-use crate::resources::KomorebiState;
-use bevy::prelude::*;
-use komorebi_client::{send_query, SocketMessage};
+ use crate::resources::KomorebiState;
+ use bevy::prelude::*;
+ use komotool_pipe::PipeNotificationEvent;
 
-pub fn fetch_komorebi_state(mut komorebi_state: ResMut<KomorebiState>) {
-    if let Ok(response) = send_query(&SocketMessage::State) {
-        if let Ok(new_state) = serde_json::from_str(&response) {
-            komorebi_state.last = komorebi_state.current.take();
-            komorebi_state.current = Some(new_state);
-        }
-    }
-}
+ pub fn update_komorebi_state_from_notifications(
+     mut komorebi_state: ResMut<KomorebiState>,
+     mut notifications: EventReader<PipeNotificationEvent>,
+ ) {
+     // Only take last notification for state update
+     if let Some(last) = notifications.read().last() {
+         komorebi_state.last = komorebi_state.current.take();
+         komorebi_state.current = Some(last.notification.state.clone());
+     }
+ }
