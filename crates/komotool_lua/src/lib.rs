71e7b6f62ef9e5bfc8e7ada99714b87bb13985cd
pro470 <reynoldsadjei@gmx.de>
2025-03-16 15:52:48 +0100

    refactor: moved to the new Startup schedule and created a insert functions for all Updates schedule for lua and rhai

--- crates/komotool_lua/src/lib.rs
+++ crates/komotool_lua/src/lib.rs
@@ -1,8 +1,10 @@
 use bevy_app::{App, Plugin, PostUpdate, PreUpdate, Update};
-use bevy_ecs::schedule::IntoSystemConfigs;
+use bevy_ecs::schedule::{IntoSystemConfigs, Schedules};
+use bevy_ecs::system::ResMut;
 use bevy_mod_scripting::core::handler::event_handler;
 use bevy_mod_scripting::lua::LuaScriptingPlugin;
 use bevy_state::condition::in_state;
+use komotool_utils::startup_schedule::{PostUpdateStartup, PreUpdateStartup, UpdateStartup};
 use komotool_utils::{prelude::*, send_event_systems::*};
 
 pub struct KomoToolLuaPlugin;
@@ -12,38 +14,36 @@ impl Plugin for KomoToolLuaPlugin {
         app.add_plugins(LuaScriptingPlugin::default())
             // Phased initialization systems
             .add_systems(
-                PreUpdate,
+                PreUpdateStartup,
                 event_handler::<OnPreStartUp, LuaScriptingPlugin>
                     .run_if(in_state(GlobalLoadingState::PreStartupDone))
                     .after(send_pre_startup_events),
             )
             .add_systems(
-                Update,
+                UpdateStartup,
                 event_handler::<OnStartUp, LuaScriptingPlugin>
                     .run_if(in_state(GlobalLoadingState::StartupDone))
                     .after(send_startup_events),
             )
             .add_systems(
-                PostUpdate,
+                PostUpdateStartup,
                 event_handler::<OnPostStartUp, LuaScriptingPlugin>
                     .run_if(in_state(GlobalLoadingState::PostStartupDone))
                     .after(send_post_startup_events),
             )
             // Add systems for the main loop phases
             .add_systems(
-                PreUpdate,
-                event_handler::<OnPreUpdate, LuaScriptingPlugin>
-                    .run_if(in_state(GlobalLoadingState::AllDone)),
-            )
-            .add_systems(
-                Update,
-                event_handler::<OnUpdate, LuaScriptingPlugin>
-                    .run_if(in_state(GlobalLoadingState::AllDone)),
-            )
-            .add_systems(
-                PostUpdate,
-                event_handler::<OnPostUpdate, LuaScriptingPlugin>
-                    .run_if(in_state(GlobalLoadingState::AllDone)),
+                UpdateStartup,
+                insert_komotool_lua_handlers.run_if(in_state(GlobalLoadingState::CleanupDone)),
             );
     }
+}
+
+pub fn insert_komotool_lua_handlers(mut schedule: ResMut<Schedules>) {
+    schedule.add_systems(PreUpdate, event_handler::<OnPreUpdate, LuaScriptingPlugin>);
+    schedule.add_systems(Update, event_handler::<OnUpdate, LuaScriptingPlugin>);
+    schedule.add_systems(
+        PostUpdate,
+        event_handler::<OnPostUpdate, LuaScriptingPlugin>,
+    );
 }
--- crates/komotool_rhai/src/lib.rs
+++ crates/komotool_rhai/src/lib.rs
@@ -1,4 +1,6 @@
 use bevy_app::{App, Plugin, PostUpdate, PreUpdate, Update};
+use bevy_ecs::change_detection::ResMut;
+use bevy_ecs::prelude::Schedules;
 use bevy_ecs::schedule::IntoSystemConfigs;
 use bevy_mod_scripting::core::handler::event_handler;
 use bevy_mod_scripting::rhai::RhaiScriptingPlugin;
@@ -7,6 +9,7 @@ use komotool_utils::prelude::*;
 use komotool_utils::send_event_systems::{
     send_post_startup_events, send_pre_startup_events, send_startup_events,
 };
+use komotool_utils::startup_schedule::{PostUpdateStartup, PreUpdateStartup, UpdateStartup};
 
 pub struct KomoToolRhaiPlugin;
 
@@ -15,38 +18,36 @@ impl Plugin for KomoToolRhaiPlugin {
         app.add_plugins(RhaiScriptingPlugin::default())
             // Phased initialization systems
             .add_systems(
-                PreUpdate,
+                PreUpdateStartup,
                 event_handler::<OnPreStartUp, RhaiScriptingPlugin>
                     .run_if(in_state(GlobalLoadingState::PreStartupDone))
                     .after(send_pre_startup_events),
             )
             .add_systems(
-                Update,
+                UpdateStartup,
                 event_handler::<OnStartUp, RhaiScriptingPlugin>
                     .run_if(in_state(GlobalLoadingState::StartupDone))
                     .after(send_startup_events),
             )
             .add_systems(
-                PostUpdate,
+                PostUpdateStartup,
                 event_handler::<OnPostStartUp, RhaiScriptingPlugin>
                     .run_if(in_state(GlobalLoadingState::PostStartupDone))
                     .after(send_post_startup_events),
             )
             // Add systems for the main loop phases
             .add_systems(
-                PreUpdate,
-                event_handler::<OnPreUpdate, RhaiScriptingPlugin>
-                    .run_if(in_state(GlobalLoadingState::AllDone)),
-            )
-            .add_systems(
-                Update,
-                event_handler::<OnUpdate, RhaiScriptingPlugin>
-                    .run_if(in_state(GlobalLoadingState::AllDone)),
-            )
-            .add_systems(
-                PostUpdate,
-                event_handler::<OnPostUpdate, RhaiScriptingPlugin>
-                    .run_if(in_state(GlobalLoadingState::AllDone)),
+                UpdateStartup,
+                insert_komotool_rhai_handlers.run_if(in_state(GlobalLoadingState::CleanupDone)),
             );
     }
+}
+
+pub fn insert_komotool_rhai_handlers(mut schedule: ResMut<Schedules>) {
+    schedule.add_systems(PreUpdate, event_handler::<OnPreUpdate, RhaiScriptingPlugin>);
+    schedule.add_systems(Update, event_handler::<OnUpdate, RhaiScriptingPlugin>);
+    schedule.add_systems(
+        PostUpdate,
+        event_handler::<OnPostUpdate, RhaiScriptingPlugin>,
+    );
 }
