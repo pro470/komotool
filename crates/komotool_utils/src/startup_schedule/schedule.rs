4745cf806f8b4b7fa90c10e8169cf0cd2f952833
pro470 <reynoldsadjei@gmx.de>
2025-03-16 15:45:07 +0100

    refactor: added startup schedule.rs

---
+++ crates/komotool_utils/src/startup_schedule/schedule.rs
@@ -0,0 +1,9 @@
+use bevy_ecs;
+use bevy_ecs::schedule::ScheduleLabel;
+
+#[derive(ScheduleLabel, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct PreUpdateStartup;
+#[derive(ScheduleLabel, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct UpdateStartup;
+#[derive(ScheduleLabel, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct PostUpdateStartup;
