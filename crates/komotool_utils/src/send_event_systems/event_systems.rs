a0b293cea6957ff73d207b9796be1b0aa2ba356b
pro470 <reynoldsadjei@gmx.de>
2025-03-16 15:51:25 +0100

    refactor: added insert function for Pre-, Post-, Update for the send functions and a new clean up next state function

--- crates/komotool_utils/src/send_event_systems/event_systems.rs
+++ crates/komotool_utils/src/send_event_systems/event_systems.rs
@@ -1,7 +1,9 @@
 use crate::{
     GlobalLoadingState, OnPostStartUp, OnPostUpdate, OnPreStartUp, OnPreUpdate, OnStartUp, OnUpdate,
 };
+use bevy_app::{Last, PreUpdate, Update};
 use bevy_ecs::event::EventWriter;
+use bevy_ecs::schedule::Schedules;
 use bevy_ecs::system::ResMut;
 use bevy_mod_scripting::core::event::ScriptCallbackEvent;
 use bevy_state::state::NextState;
@@ -31,6 +33,10 @@ pub fn send_post_startup_events(
     next_state.set(GlobalLoadingState::PostStartupDone);
 }
 
+pub fn advance_to_clean_up_done(mut next_state: ResMut<NextState<GlobalLoadingState>>) {
+    next_state.set(GlobalLoadingState::CleanupDone);
+}
+
 pub fn advance_to_all_done(mut next_state: ResMut<NextState<GlobalLoadingState>>) {
     next_state.set(GlobalLoadingState::AllDone);
 }
@@ -46,4 +52,10 @@ pub fn send_update_events(mut writer: EventWriter<ScriptCallbackEvent>) {
 
 pub fn send_post_update_events(mut writer: EventWriter<ScriptCallbackEvent>) {
     writer.send(ScriptCallbackEvent::new_for_all(OnPostUpdate, vec![]));
+}
+
+pub fn insert_event_sending_systems(mut schedule: ResMut<Schedules>) {
+    schedule.add_systems(Last, send_pre_update_events);
+    schedule.add_systems(PreUpdate, send_update_events);
+    schedule.add_systems(Update, send_post_update_events);
 }
