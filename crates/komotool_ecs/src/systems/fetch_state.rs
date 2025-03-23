f80b41f7e8ca86a24998216f51579aad5983c24f
pro470 <reynoldsadjei@gmx.de>
2025-03-23 17:53:34 +0100

    refactor: removed the import of serde_json

--- crates/komotool_ecs/src/systems/fetch_state.rs
+++ crates/komotool_ecs/src/systems/fetch_state.rs
@@ -2,7 +2,6 @@ use crate::resources::KomorebiState;
 use bevy_ecs::event::EventReader;
 use bevy_ecs::system::ResMut;
 use komotool_pipe::PipeNotificationEvent;
-use serde_json::{from_value, to_value};
 
 pub fn update_komorebi_state_from_notifications(
     mut komorebi_state: ResMut<KomorebiState>,
