b9cbbde1f08166b23943450b81ce598474736df9
pro470 <reynoldsadjei@gmx.de>
2025-04-14 17:13:34 +0200

    feat: added the idle reset to update_komorebi_state_from_notifications so the performance goes up when the state changed

--- crates/komotool_ecs/src/systems/fetch_state.rs
+++ crates/komotool_ecs/src/systems/fetch_state.rs
@@ -1,11 +1,14 @@
 use crate::resources::KomorebiState;
 use bevy_ecs::event::EventReader;
 use bevy_ecs::system::ResMut;
+use bevy_utils::Instant;
+use komotool_framepace::IdleFramePaceState;
 use komotool_pipe::PipeNotificationEvent;
 
 pub fn update_komorebi_state_from_notifications(
     mut komorebi_state: ResMut<KomorebiState>,
     mut notifications: EventReader<PipeNotificationEvent>,
+    mut idle: ResMut<IdleFramePaceState>,
 ) {
     // Take last notification
     if let Some(last) = notifications.read().last() {
@@ -13,6 +16,7 @@ pub fn update_komorebi_state_from_notifications(
             if state.has_been_modified(&last.notification.state) {
                 println!("State has been modified");
                 komorebi_state.komorebi = Some(last.notification.state.clone());
+                idle.last_activity = Instant::now();
             }
         } else {
             komorebi_state.komorebi = Some(last.notification.state.clone());
